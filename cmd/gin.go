fbfedb9eaa572690d26b29531f199cc32a7ef1e5
zhouyi3 <zhouyi3@lixiang.com>
2023-09-23 12:55:19 +0800

    first commit

--- cmd/gin.go
+++ cmd/gin.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"time"
+	// "time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
-		print("收到")
-		time.Sleep(5 * time.Second)
 
 		c.JSON(200, gin.H{
 			"message": "pong",
--- main.go
+++ main.go
@@ -23,4 +23,5 @@ func main() {
 	default:
 		panic(ctx.Command())
 	}
+
 }
